2cbda833cf60361dd89fe17a832092a646abf36b
lauranogales <ln120@ic.ac.uk>
2022-10-19 16:22:19 +0100

    Initial commit

---
+++ src/main/java/Drawing.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+public class Drawing extends Canvas {
+    Point p = new Point(200,200);
+    Color c = new Color(0x82266);
+    private Circle circ ;
+    private Frame f;
+    public Drawing(){
+        f = new Frame("My window");
+        f.add(this);
+        f.setLayout(null);
+        f.setSize(400,400);
+        f.setVisible(true);
+        f.addWindowListener(new WindowAdapter(){
+            public void windowClosing(WindowEvent e){
+                f.dispose();
+            }
+        });
+        setBackground(Color.WHITE);
+        setSize(400,400);
+    }
+    public void paint(Graphics g){
+        circ = new Circle(p,c,100);
+        circ.draw(g);
+    }
+
+}
